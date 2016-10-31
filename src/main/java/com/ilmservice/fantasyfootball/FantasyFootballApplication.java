a7488ef50472eb981b93c4754cef2dca33d7f283
jolson490 <joshua.olson.490@gmail.com>
2016-10-31 14:47:39 -0500

    Change to a Spring Boot app that was generated with Spring Initializr (start.spring.io). (In pom.xml: let spring-boot pick the version for most dependencies, step-up derby version from 10.11 to 10.12, change the groupId and artifactId.)

---
+++ src/main/java/com/ilmservice/fantasyfootball/FantasyFootballApplication.java
@@ -0,0 +1,12 @@
+package com.ilmservice.fantasyfootball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyFootballApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FantasyFootballApplication.class, args);
+  }
+}
