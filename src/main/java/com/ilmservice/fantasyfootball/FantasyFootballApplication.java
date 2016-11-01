5d104788048621dd8c8c91c90e2b41b84364140e
jolson490 <joshua.olson.490@gmail.com>
2016-11-01 17:20:42 -0500

    giving up trying to log from main.

--- src/main/java/com/ilmservice/fantasyfootball/FantasyFootballApplication.java
+++ src/main/java/com/ilmservice/fantasyfootball/FantasyFootballApplication.java
@@ -1,31 +1,22 @@
 package com.ilmservice.fantasyfootball;
 
-import java.util.Arrays;
+import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FantasyFootballApplication {
   private static final Logger logger = LoggerFactory.getLogger(FantasyFootballApplication.class);
 
   public static void main(String[] args) {
-    logger.debug("begin main()");
-    System.out.println("begin main() - System.out.println");
+    SpringApplication.run(FantasyFootballApplication.class, args);
+  }
 
-    ApplicationContext ctx = SpringApplication.run(FantasyFootballApplication.class, args);
-    logger.debug("Let's inspect the beans provided by Spring Boot:");
-    String[] beanNames = ctx.getBeanDefinitionNames();
-    Arrays.sort(beanNames);
-    for (String beanName : beanNames) {
-      logger.debug(beanName);
-    }
-
-    // TODO why not get output?
-    logger.debug("end main");
-    System.out.println("end main - System.out.println");
+  @PostConstruct
+  public void logSomething() {
+    logger.debug("in logSomething()");
   }
 }
