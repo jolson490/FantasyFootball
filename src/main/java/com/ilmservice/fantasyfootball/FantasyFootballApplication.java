6ed0a5e86a1b0fba08e6e93bead3de21b706e439
jolson490 <joshua.olson.490@gmail.com>
2016-12-01 13:48:56 -0600

    try to fix 404 error page when start app via: Run As, Spring Boot App.

--- src/main/java/com/ilmservice/fantasyfootball/FantasyFootballApplication.java
+++ src/main/java/com/ilmservice/fantasyfootball/FantasyFootballApplication.java
@@ -7,16 +7,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "com.ilmservice.fantasyfootball.db.entities" })
 @EnableJpaRepositories(basePackages = { "com.ilmservice.fantasyfootball.db.repositories" })
-public class FantasyFootballApplication {
+public class FantasyFootballApplication extends SpringBootServletInitializer {
   private static final Logger logger = LoggerFactory.getLogger(FantasyFootballApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(FantasyFootballApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(FantasyFootballApplication.class);
   }
 
   @PostConstruct
