85c88f59026ac675a4c27422d5ebd6137356b4e2
jolson490 <joshua.olson.490@gmail.com>
2016-12-12 15:49:19 -0600

    In NFLTeam, get rid of reference to Player (I am not sure why eclipse JPA plugin thought this should be a bi-directional OneToMany association). In Player, make updates per previous commit c30d53b - change nflTeam field from (unrestricted) String to (OneToOne) NFLTeam class.

--- src/main/java/com/ilmservice/fantasyfootball/db/entities/NFLTeam.java
+++ src/main/java/com/ilmservice/fantasyfootball/db/entities/NFLTeam.java
@@ -1,51 +1,36 @@
 package com.ilmservice.fantasyfootball.db.entities;
 
-import java.io.Serializable;
-import java.util.List;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
-/**
- * The persistent class for the NFLTEAMS database table.
- *
- */
 @Entity
-@Table(name="NFLTEAMS")
-@NamedQuery(name="NFLTeam.findAll", query="SELECT n FROM NFLTeam n")
-public class NFLTeam implements Serializable {
-  private static final long serialVersionUID = 1L;
+@Table(name = "nflTeams")
+public class NFLTeam {
 
   @Id
-  private String locationabbreviation;
+  private String locationAbbreviation;
 
   private String location;
 
   private String mascot;
 
-  //bi-directional many-to-one association to Player
-  @OneToMany(mappedBy="nflteamBean")
-  private List<Player> players;
-
-  public NFLTeam() {
-  }
+  /*
+   * //bi-directional many-to-one association to Player
+   * 
+   * @OneToMany(mappedBy="nflteamBean") private List<Player> players;
+   */
 
   public String getLocationabbreviation() {
-    return this.locationabbreviation;
+    return this.locationAbbreviation;
   }
-
   public void setLocationabbreviation(String locationabbreviation) {
-    this.locationabbreviation = locationabbreviation;
+    this.locationAbbreviation = locationabbreviation;
   }
 
   public String getLocation() {
     return this.location;
   }
-
   public void setLocation(String location) {
     this.location = location;
   }
@@ -53,31 +38,23 @@ public class NFLTeam implements Serializable {
   public String getMascot() {
     return this.mascot;
   }
-
   public void setMascot(String mascot) {
     this.mascot = mascot;
   }
 
-  public List<Player> getPlayers() {
-    return this.players;
-  }
-
-  public void setPlayers(List<Player> players) {
-    this.players = players;
-  }
-
-  public Player addPlayer(Player player) {
-    getPlayers().add(player);
-    player.setNflteamBean(this);
-
-    return player;
-  }
-
-  public Player removePlayer(Player player) {
-    getPlayers().remove(player);
-    player.setNflteamBean(null);
-
-    return player;
-  }
-
+  /*
+   * public List<Player> getPlayers() { return this.players; }
+   * 
+   * public void setPlayers(List<Player> players) { this.players = players; }
+   * 
+   * public Player addPlayer(Player player) { getPlayers().add(player);
+   * player.setNflteamBean(this);
+   * 
+   * return player; }
+   * 
+   * public Player removePlayer(Player player) { getPlayers().remove(player);
+   * player.setNflteamBean(null);
+   * 
+   * return player; }
+   */
 }
--- src/main/java/com/ilmservice/fantasyfootball/db/entities/Player.java
+++ src/main/java/com/ilmservice/fantasyfootball/db/entities/Player.java
@@ -5,14 +5,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "Players")
 public class Player {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "ID", nullable = false, unique = true, length = 11)
+  @Column(name = "ID")
   private int id;
 
   @Column(name = "FNAME")
@@ -24,16 +27,16 @@ public class Player {
   @Column(name = "POSITION")
   String position;
 
-  @Column(name = "NFLTEAM")
-  String nflTeam;
+  @OneToOne
+  @JoinColumn(name = "NFLTEAM")
+  NFLTeam nflTeam;
 
   @Column(name = "POSITIONRANKING")
-  int positionRnking;
+  int positionRanking;
 
   public int getId() {
     return id;
   }
-
   public void setId(int id) {
     this.id = id;
   }
@@ -41,7 +44,6 @@ public class Player {
   public String getFirstName() {
     return firstName;
   }
-
   public void setFirstName(String firstName) {
     this.firstName = firstName;
   }
@@ -49,7 +51,6 @@ public class Player {
   public String getLastName() {
     return lastName;
   }
-
   public void setLastName(String lastName) {
     this.lastName = lastName;
   }
@@ -57,9 +58,21 @@ public class Player {
   public String getPosition() {
     return position;
   }
-
   public void setPosition(String position) {
     this.position = position;
   }
 
+  public NFLTeam getNflTeam() {
+    return nflTeam;
+  }
+  public void setNflTeam(NFLTeam nflTeam) {
+    this.nflTeam = nflTeam;
+  }
+
+  public int getPositionRanking() {
+    return positionRanking;
+  }
+  public void setPositionRanking(int positionRanking) {
+    this.positionRanking = positionRanking;
+  }
 }
